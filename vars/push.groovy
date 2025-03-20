d137de7de76d5b8cd8843b00e0c1bb7ca7f958f6
Manvanth mn <92145905+Manvanthmanu@users.noreply.github.com>
2025-03-20 23:42:42 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,6 +1,6 @@
-def call(String ID, String imageName, String ImageVersion ){
+def call(String ID, String imageName, String imageVersion ){
   withCredentials([usernamePassword('credentialsId':"${ID}", passwordVariable:"dockerhubPass", usernameVariable:"dockerhubUser")]){
                     sh "docker login -u ${env.dockerHubUser} -p ${env.dockerhubPass}"
-                    sh "docker image tag ${ImageName}:${ImageVersion} ${env.dockerHubUser}/${ImageName}:${ImageVersion}"
-                    sh "docker push ${env.dockerHubUser}/${ImageName}:${ImageVersion}"
+                    sh "docker image tag ${imageName}:${imageVersion} ${env.dockerHubUser}/${imageName}:${imageVersion}"
+                    sh "docker push ${env.dockerHubUser}/${imageName}:${imageVersion}"
 }
